918a96692c698200e1cd03de005c984eadefdad7
egrodo <egrodo1@gmail.com>
2018-12-09 15:53:17 -0500

    Updates

---
+++ precache-manifest.060f7015995b5a97126f02e597411a45.js
@@ -0,0 +1,306 @@
+self.__precacheManifest = [
+  {
+    "revision": "92c0d6ef479ede592ceb5a0e5d0c99d0",
+    "url": "https://choochoo.app/static/media/cog.92c0d6ef.svg"
+  },
+  {
+    "revision": "7d4acbca42f0bab6ce2a",
+    "url": "https://choochoo.app/static/css/main.74d3d5e9.chunk.css"
+  },
+  {
+    "revision": "9657ea33c88b6cccee71",
+    "url": "https://choochoo.app/static/js/1.9657ea33.chunk.js"
+  },
+  {
+    "revision": "f7263b07084d71b7f13b",
+    "url": "https://choochoo.app/static/js/2.f7263b07.chunk.js"
+  },
+  {
+    "revision": "d60a998e28f4357fef7f",
+    "url": "https://choochoo.app/static/js/3.d60a998e.chunk.js"
+  },
+  {
+    "revision": "993ebffab1980b410a67",
+    "url": "https://choochoo.app/static/js/4.993ebffa.chunk.js"
+  },
+  {
+    "revision": "f4937c21d9e1f260783f",
+    "url": "https://choochoo.app/static/js/5.f4937c21.chunk.js"
+  },
+  {
+    "revision": "549d4eefef1d22edb0c0",
+    "url": "https://choochoo.app/static/js/6.549d4eef.chunk.js"
+  },
+  {
+    "revision": "f082391ea0c5800e8d38",
+    "url": "https://choochoo.app/static/js/7.f082391e.chunk.js"
+  },
+  {
+    "revision": "9bbb075ec86a220364ed",
+    "url": "https://choochoo.app/static/js/8.9bbb075e.chunk.js"
+  },
+  {
+    "revision": "9cfe6bb02ac9757cb9c9",
+    "url": "https://choochoo.app/static/js/9.9cfe6bb0.chunk.js"
+  },
+  {
+    "revision": "3fdf0e3ba1f8d5f14fcc",
+    "url": "https://choochoo.app/static/js/10.3fdf0e3b.chunk.js"
+  },
+  {
+    "revision": "7845e1d968e0edcf377e",
+    "url": "https://choochoo.app/static/js/11.7845e1d9.chunk.js"
+  },
+  {
+    "revision": "f1e8784c366388657158",
+    "url": "https://choochoo.app/static/js/12.f1e8784c.chunk.js"
+  },
+  {
+    "revision": "9cdeb3c79c982e44f738",
+    "url": "https://choochoo.app/static/js/13.9cdeb3c7.chunk.js"
+  },
+  {
+    "revision": "cf2cca926bb3e42d9291",
+    "url": "https://choochoo.app/static/js/14.cf2cca92.chunk.js"
+  },
+  {
+    "revision": "32a4434e47abe55c725d",
+    "url": "https://choochoo.app/static/js/15.32a4434e.chunk.js"
+  },
+  {
+    "revision": "04b2e56402fb76e73488",
+    "url": "https://choochoo.app/static/js/16.04b2e564.chunk.js"
+  },
+  {
+    "revision": "5fd076b05fe16ad9a56e",
+    "url": "https://choochoo.app/static/js/17.5fd076b0.chunk.js"
+  },
+  {
+    "revision": "4a89615da751dad32120",
+    "url": "https://choochoo.app/static/js/18.4a89615d.chunk.js"
+  },
+  {
+    "revision": "b79b101481c3ffdf21b1",
+    "url": "https://choochoo.app/static/js/19.b79b1014.chunk.js"
+  },
+  {
+    "revision": "e12114772ca872147761",
+    "url": "https://choochoo.app/static/js/20.e1211477.chunk.js"
+  },
+  {
+    "revision": "c8696e8459925560e9e1",
+    "url": "https://choochoo.app/static/js/21.c8696e84.chunk.js"
+  },
+  {
+    "revision": "357fe7baa19d4fb344e0",
+    "url": "https://choochoo.app/static/js/22.357fe7ba.chunk.js"
+  },
+  {
+    "revision": "43219fd930c2668544c6",
+    "url": "https://choochoo.app/static/js/23.43219fd9.chunk.js"
+  },
+  {
+    "revision": "6775089796287ecc0adc",
+    "url": "https://choochoo.app/static/js/24.67750897.chunk.js"
+  },
+  {
+    "revision": "b1b96752944a1c3d4ddc",
+    "url": "https://choochoo.app/static/js/25.b1b96752.chunk.js"
+  },
+  {
+    "revision": "f38c35455e992ef858be",
+    "url": "https://choochoo.app/static/js/26.f38c3545.chunk.js"
+  },
+  {
+    "revision": "c632e2b3486effef7fd9",
+    "url": "https://choochoo.app/static/js/27.c632e2b3.chunk.js"
+  },
+  {
+    "revision": "80320e386fe60ad02d26",
+    "url": "https://choochoo.app/static/js/28.80320e38.chunk.js"
+  },
+  {
+    "revision": "7ac2cdb8ce28bb9e41a7",
+    "url": "https://choochoo.app/static/js/29.7ac2cdb8.chunk.js"
+  },
+  {
+    "revision": "c3159977343785a48cd1",
+    "url": "https://choochoo.app/static/js/30.c3159977.chunk.js"
+  },
+  {
+    "revision": "b9c839e8eeb42943ee29839e80eb3e84",
+    "url": "https://choochoo.app/static/media/z.b9c839e8.svg"
+  },
+  {
+    "revision": "40ada850e4984cf0ee83",
+    "url": "https://choochoo.app/static/js/31.40ada850.chunk.js"
+  },
+  {
+    "revision": "a9618711bc54a31cb94c",
+    "url": "https://choochoo.app/static/js/runtime~main.a9618711.js"
+  },
+  {
+    "revision": "da9ebc967a80f5ebdc3609c859c46494",
+    "url": "https://choochoo.app/static/media/upArrow.da9ebc96.svg"
+  },
+  {
+    "revision": "bb93e3a2bf87cd2035f1215f4bea8aa3",
+    "url": "https://choochoo.app/static/media/cancel.bb93e3a2.svg"
+  },
+  {
+    "revision": "eab30f6eda703fa98429dcfaf13283b7",
+    "url": "https://choochoo.app/static/media/spinner.eab30f6e.svg"
+  },
+  {
+    "revision": "7d4acbca42f0bab6ce2a",
+    "url": "https://choochoo.app/static/js/main.7d4acbca.chunk.js"
+  },
+  {
+    "revision": "eae9c18cee82a8a1a52e654911f8fe83",
+    "url": "https://choochoo.app/static/media/Product Sans Regular.eae9c18c.ttf"
+  },
+  {
+    "revision": "dba0c688b8d5ee09a1e214aebd5d25e4",
+    "url": "https://choochoo.app/static/media/Product Sans Bold.dba0c688.ttf"
+  },
+  {
+    "revision": "275bbde95ab10333568082f6bd424931",
+    "url": "https://choochoo.app/static/media/sunny-day.275bbde9.png"
+  },
+  {
+    "revision": "390291fc49ccae5bbb824d84244d298a",
+    "url": "https://choochoo.app/static/media/whiteTrain.390291fc.png"
+  },
+  {
+    "revision": "ac575565b3fc06e79013be331dca3f03",
+    "url": "https://choochoo.app/static/media/emailIcon.ac575565.svg"
+  },
+  {
+    "revision": "941f4bce161059bfebc747283e53ccc2",
+    "url": "https://choochoo.app/static/media/cloudIcon.941f4bce.svg"
+  },
+  {
+    "revision": "5e8d8a626c493a81ed2a16229241083c",
+    "url": "https://choochoo.app/static/media/1.5e8d8a62.svg"
+  },
+  {
+    "revision": "2404f20df4d3fa94561aae51c45fee1b",
+    "url": "https://choochoo.app/static/media/2.2404f20d.svg"
+  },
+  {
+    "revision": "c1a3b57dfbbe9b4a82a16a6f6b6861cd",
+    "url": "https://choochoo.app/static/media/3.c1a3b57d.svg"
+  },
+  {
+    "revision": "d9de3d7f6fe286bac80761f1f8d98047",
+    "url": "https://choochoo.app/static/media/4.d9de3d7f.svg"
+  },
+  {
+    "revision": "4609e23a60217cc05e63c661a184892b",
+    "url": "https://choochoo.app/static/media/5.4609e23a.svg"
+  },
+  {
+    "revision": "4609e23a60217cc05e63c661a184892b",
+    "url": "https://choochoo.app/static/media/5x.4609e23a.svg"
+  },
+  {
+    "revision": "9c2efe6e8f75edbd445c148a25c05b45",
+    "url": "https://choochoo.app/static/media/6.9c2efe6e.svg"
+  },
+  {
+    "revision": "9c2efe6e8f75edbd445c148a25c05b45",
+    "url": "https://choochoo.app/static/media/6x.9c2efe6e.svg"
+  },
+  {
+    "revision": "2466a4be8e60b04bc1e0748a4ed7b133",
+    "url": "https://choochoo.app/static/media/7.2466a4be.svg"
+  },
+  {
+    "revision": "2466a4be8e60b04bc1e0748a4ed7b133",
+    "url": "https://choochoo.app/static/media/7x.2466a4be.svg"
+  },
+  {
+    "revision": "c628edff03bec7e5265114a7bae4aac4",
+    "url": "https://choochoo.app/static/media/a.c628edff.svg"
+  },
+  {
+    "revision": "1c1a4bf2b55a67d5d90763a50e6b6bbb",
+    "url": "https://choochoo.app/static/media/b.1c1a4bf2.svg"
+  },
+  {
+    "revision": "0f67a404466d2f799011d5b9a0c511e0",
+    "url": "https://choochoo.app/static/media/c.0f67a404.svg"
+  },
+  {
+    "revision": "493b55b3910ab11fd7ec26b22aad59d2",
+    "url": "https://choochoo.app/static/media/d.493b55b3.svg"
+  },
+  {
+    "revision": "c92c57bce4077fc90c92c158962ad940",
+    "url": "https://choochoo.app/static/media/e.c92c57bc.svg"
+  },
+  {
+    "revision": "9c885cbb379534cf1df6571c490ca860",
+    "url": "https://choochoo.app/static/media/f.9c885cbb.svg"
+  },
+  {
+    "revision": "9cbdb37737b98f1eace66d86999f97b6",
+    "url": "https://choochoo.app/static/media/g.9cbdb377.svg"
+  },
+  {
+    "revision": "daa784afe24c10506f72106d7e467921",
+    "url": "https://choochoo.app/static/media/gs.daa784af.svg"
+  },
+  {
+    "revision": "29e1ec3b494d3422cd535e6149face84",
+    "url": "https://choochoo.app/static/media/h.29e1ec3b.svg"
+  },
+  {
+    "revision": "b0f72cc6c2d94a88409899152e7571c9",
+    "url": "https://choochoo.app/static/media/j.b0f72cc6.svg"
+  },
+  {
+    "revision": "2f08bb1936c628c6cb9fd3c047462801",
+    "url": "https://choochoo.app/static/media/l.2f08bb19.svg"
+  },
+  {
+    "revision": "ebcda059e0588f4567d34a8114f0e544",
+    "url": "https://choochoo.app/static/media/m.ebcda059.svg"
+  },
+  {
+    "revision": "96d1aab73a5195ee8a9e30af2e152d9f",
+    "url": "https://choochoo.app/static/media/n.96d1aab7.svg"
+  },
+  {
+    "revision": "48f0166a67165c10793143cc2f74e133",
+    "url": "https://choochoo.app/static/media/q.48f0166a.svg"
+  },
+  {
+    "revision": "55f52aa73277e49ea85e72acbbc722b7",
+    "url": "https://choochoo.app/static/media/r.55f52aa7.svg"
+  },
+  {
+    "revision": "daa784afe24c10506f72106d7e467921",
+    "url": "https://choochoo.app/static/media/s.daa784af.svg"
+  },
+  {
+    "revision": "0a7812d847a8f78edb8f7b06ba0dc081",
+    "url": "https://choochoo.app/static/media/si.0a7812d8.svg"
+  },
+  {
+    "revision": "6900c590d1b2486837728be44e6673a1",
+    "url": "https://choochoo.app/static/media/t.6900c590.svg"
+  },
+  {
+    "revision": "7a7b53924ac6f6423fa88680792403bc",
+    "url": "https://choochoo.app/static/media/w.7a7b5392.svg"
+  },
+  {
+    "revision": "40ada850e4984cf0ee83",
+    "url": "https://choochoo.app/static/css/31.c198ff71.chunk.css"
+  },
+  {
+    "revision": "7cfea9eded96b772a96b411b97ed0dff",
+    "url": "https://choochoo.app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f7f292c4b6fe29392e0b6f813d197759.js"
+  "https://choochoo.app/precache-manifest.060f7015995b5a97126f02e597411a45.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("https://choochoo.app/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
